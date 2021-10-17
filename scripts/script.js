a92bc39e357169c7d70544ed9ddf59ebfc161c05
Yuriy Balashov <balashov.yuv@yandex.ru>
2021-10-17 16:20:01 +0300

    версия перед первым ревью

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+let formElement = document.querySelector('.profile__description');
+let nameInput = formElement.querySelector('.profile__title');
+let jobInput = formElement.querySelector('.profile__profession');
+let editProfileButton = document.querySelector('.profile__edit-button');
+let closePopupButton = document.querySelector('.popup__close');
+let popupName = document.querySelector('.popup__title');
+let popupProfession = document.querySelector('.popup__profession');
+let popup = document.querySelector('.popup');
+let form = document.querySelector('.popup__container');
+
+function popupOpen() {
+  popupName.value = nameInput.textContent;
+  popupProfession.value = jobInput.textContent;
+  popup.classList.add('popup_open');
+}
+
+function popupClose() {
+  popup.classList.remove('popup_open');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameInput.textContent = popupName.value;
+  jobInput.textContent = popupProfession.value;
+  popupClose();
+}
+
+form.addEventListener('submit', formSubmitHandler);
+editProfileButton.addEventListener('click', popupOpen);
+closePopupButton.addEventListener('click', popupClose);
