ba3a26ce9d77885ad5e40c5cee3de9f45c6c2247
Yuriy Balashov <balashov.yuv@yandex.ru>
2021-11-12 22:03:35 +0300

    изменение места хранения изображений на массив

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let formElement = document.querySelector('.profile__description');
+let elements = document.querySelector('.elements');
 let nameInput = formElement.querySelector('.profile__title');
 let jobInput = formElement.querySelector('.profile__profession');
 let editProfileButton = document.querySelector('.profile__edit-button');
@@ -7,6 +8,37 @@ let popupName = document.querySelector('.popup__input_type_title');
 let popupProfession = document.querySelector('.popup__input_type_profession');
 let popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__container');
+let addPictureButton = document.querySelector('.profile__add-button');
+let itemTemplate = document.querySelector('.card-template').content;
+let deleteCardIcon = document.querySelector('.elements__delete');
+
+
+const initialCards = [
+  {
+    name: 'Азовское море',
+    link: './images/azov.jpg'
+  },
+  {
+    name: 'Казань',
+    link: './images/kazan.jpg'
+  },
+  {
+    name: 'Сочи',
+    link: './images/sochi.jpg'
+  },
+  {
+    name: 'Волгоград',
+    link: './images/volgograd.jpg'
+  },
+  {
+    name: 'Лаго-Наки',
+    link: './images/lagonaki.jpg'
+  },
+  {
+    name: 'Нижний Новгород',
+    link: './images/nn.jpg'
+  }
+];
 
 function popupOpen() {
   popupName.value = nameInput.textContent;
@@ -14,8 +46,21 @@ function popupOpen() {
   popup.classList.add('popup_open');
 }
 
+initialCards.forEach(function (element) {
+  const cardElement = itemTemplate.cloneNode(true);
+  cardElement.querySelector('.elements__image').src = element.link;
+  cardElement.querySelector('.elements__mesto').textContent = element.name;
+  elements.append(cardElement);
+}) 
+
 function popupClose() {
   popup.classList.remove('popup_open');
+}
+
+function deleteCard() {
+  console.log('click');
+  const delItem = deleteCardIcon.closest('.elements__element');  
+  delItem.remove();
 }
 
 function formSubmitHandler(evt) {
@@ -28,3 +73,5 @@ function formSubmitHandler(evt) {
 form.addEventListener('submit', formSubmitHandler);
 editProfileButton.addEventListener('click', popupOpen);
 closePopupButton.addEventListener('click', popupClose);
+addPictureButton.addEventListener('click', popupOpen);
+deleteCardIcon.addEventListener('click', deleteCard);
